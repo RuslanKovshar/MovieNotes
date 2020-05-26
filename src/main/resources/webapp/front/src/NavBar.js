06d35c9e7412e3d5e9aad23c9c8c06cb3facc123
Ruslan <ruslan.kovshar@gmail.com>
2020-05-26 21:44:50 +0300

    removed login and registration links from NavBar.js

--- src/main/resources/webapp/front/src/NavBar.js
+++ src/main/resources/webapp/front/src/NavBar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import {createBrowserHistory} from "history";
 import {Redirect} from "react-router";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 
 class NavBar extends React.Component {
-    customHistory;
 
     constructor(props) {
         super(props);
@@ -15,9 +15,6 @@ class NavBar extends React.Component {
         }
     }
 
-    componentDidMount() {
-        this.customHistory = createBrowserHistory();
-    }
 
     logoutHandler = (e) => {
         e.preventDefault();
@@ -34,21 +31,16 @@ class NavBar extends React.Component {
                         :
                         <Navbar bg="dark" variant="dark">
                             <Navbar.Brand href="/">RNotes</Navbar.Brand>
-                            <Nav>
+                            <Nav className="mr-auto">
                                 <Nav.Link href="/">Home</Nav.Link>
-                            </Nav>
-                            <Nav>
-                                <Nav.Link href="/registration">Registration</Nav.Link>
-                            </Nav>
-                            <Nav>
-                                <Nav.Link href="/login">Login</Nav.Link>
                             </Nav>
 
                             {
                                 localStorage.getItem('token') !== null
-                                    ? <form onSubmit={this.logoutHandler}>
-                                        <button type='submit'>Logout</button>
-                                    </form>
+                                    ?
+                                        <Form inline onSubmit={this.logoutHandler}>
+                                            <Button variant="danger" type={"submit"}>Logout</Button>
+                                        </Form>
                                     : null
                             }
                         </Navbar>
